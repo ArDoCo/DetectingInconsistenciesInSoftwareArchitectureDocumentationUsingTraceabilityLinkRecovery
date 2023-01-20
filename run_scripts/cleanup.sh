c788f654f62d12f3d8264871864e671687cd59e0
Dominik Fuchß <dominik.fuchss@kit.edu>
2023-01-20 16:26:20 +0100

    Fix cleanup script

--- run_scripts/cleanup.sh
+++ run_scripts/cleanup.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 rm -rf ../results
-rm -rf ../approach/tests/src/test/resources/testout
+rm -rf ../approach/tests/src/test/resources/testout && mkdir -p ../approach/tests/src/test/resources/testout
