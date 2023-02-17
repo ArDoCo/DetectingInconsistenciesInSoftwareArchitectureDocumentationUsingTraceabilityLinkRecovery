fd4ac8c27b42e7cff3497ef4b8f1d51e18c9f072
Dominik Fuchß <dominik.fuchss@kit.edu>
2023-02-17 13:57:14 +0100

    Update 1_run_swattr.sh

--- swattr/1_run_swattr.sh
+++ swattr/1_run_swattr.sh
@@ -4,7 +4,6 @@ java -jar ardoco-core-pipeline.jar -c config.txt -n BigBlueButton -m owl/bbb.rep
 java -jar ardoco-core-pipeline.jar -c config.txt -n JabRef -m owl/jabref.repository.owl -t ../benchmark/jabref/text_2021/jabref.txt -o ../results/swattr-raw/jabref
 java -jar ardoco-core-pipeline.jar -c config.txt -n JabRef -m owl/jabref.repository.owl -t ../benchmark/jabref/text_2016/jabref_2016.txt -o ../results/swattr-raw/jabref-historic
 java -jar ardoco-core-pipeline.jar -c config.txt -n Mediastore -m owl/mediastore.repository.owl -t ../benchmark/mediastore/text_2016/mediastore.txt -o ../results/swattr-raw/mediastore
-java -jar ardoco-core-pipeline.jar -c config.txt -n Mediastore -m owl/mediastore.repository.owl -t ../benchmark/mediastore/text_2011/mediastore_2011.txt -o ../results/swattr-raw/mediastore-historic
 java -jar ardoco-core-pipeline.jar -c config.txt -n TEAMMATES -m owl/teammates.repository.owl -t ../benchmark/teammates/text_2021/teammates.txt -o ../results/swattr-raw/teammates
 java -jar ardoco-core-pipeline.jar -c config.txt -n TEAMMATES -m owl/teammates.repository.owl -t ../benchmark/teammates/text_2015/teammates_2015.txt -o ../results/swattr-raw/teammates-historic
 java -jar ardoco-core-pipeline.jar -c config.txt -n TeaStore -m owl/teastore.repository.owl -t ../benchmark/teastore/text_2020/teastore.txt -o ../results/swattr-raw/teastore
